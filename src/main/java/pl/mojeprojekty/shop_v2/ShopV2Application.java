e2eb6a7e9381c1ee836e28beb8cabe3a034c482f
mateu <mateusz.bialos@gmail.com>
2019-11-20 17:37:11 +0100

    Initial commit

---
+++ src/main/java/pl/mojeprojekty/shop_v2/ShopV2Application.java
@@ -0,0 +1,13 @@
+package pl.mojeprojekty.shop_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mojeprojekty/shop_v2/ShopV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mojeprojekty.shop_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
